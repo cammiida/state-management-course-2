8a6ba2633751fc59516a3948c834ab4e62cade51
Camilla <camillamdalan@gmail.com>
2022-03-29 16:37:42 +0200

    Make code ready for doing the tasks

--- src/sections/1localState/PostSearch.tsx
+++ src/sections/1localState/PostSearch.tsx
@@ -1,31 +1,12 @@
-import React, { useState } from "react";
+import React from "react";
 import useFetch from "../../hooks/useFetch";
 import { Post } from "../../types";
 import styles from "./styles.module.css";
 
 const PostSearch = () => {
-  const [searchVal, setSearchVal] = useState("");
-
-  const {
-    data: posts,
-    loading,
-    error,
-  } = useFetch(!!searchVal ? `/posts?userId=${searchVal}` : "/posts");
-
-  const handleSearchChange = (event: React.ChangeEvent<HTMLInputElement>) => {
-    console.log("handleSearchChange: ", event);
-    setSearchVal(event.currentTarget.value);
-  };
+  const { data: posts, loading, error } = useFetch("/posts");
 
   const createContent = () => {
-    if (loading) return "Loading...";
-    if (error) {
-      return (
-        <p style={{ color: "red" }}>
-          Something went wrong. The following error occurred: {error}
-        </p>
-      );
-    }
     if (posts && posts.length) {
       return posts.map((post: Post) => (
         <div className={styles.card} key={post.id}>
@@ -36,12 +17,11 @@ const PostSearch = () => {
         </div>
       ));
     }
-    return `No posts found for user with id "${searchVal}"`;
+    return `No posts found`;
   };
 
   return (
     <>
-      <input value={searchVal} onChange={handleSearchChange} />
       <div className={styles.contentContainer}>{createContent()}</div>
     </>
   );
